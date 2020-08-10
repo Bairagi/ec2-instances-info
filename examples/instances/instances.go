3fa9e99cf5e0d74f8ce458462cec6f2789ac0de9
Nileshb <bairagin@tabcorp.com.au>
2020-08-10 17:27:05 +1000

    update instace data

--- examples/instances/instances.go
+++ examples/instances/instances.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/cristim/ec2-instances-info"
+	"github.com/Bairagi/ec2-instances-info"
 )
 
 func main() {
@@ -17,30 +17,32 @@ func main() {
 	}
 
 	for _, i := range *data {
-		fmt.Print(
-			"Instance type: ", i.InstanceType,
-			",\tCPU: ", i.PhysicalProcessor,
-			",\tCPU cores: ", i.VCPU,
-			",\tMemory(GB): ", i.Memory,
-			",\tEBS Throughput(MB/s): ", i.EBSThroughput,
-			",\tcost in us-east-1: ", i.Pricing["us-east-1"].Linux.OnDemand,
-			",\tcost in af-south-1: ")
+		// fmt.Print(
+		// 	"Instance type: ", i.InstanceType,
+		// 	",\tCPU: ", i.PhysicalProcessor,
+		// 	",\tCPU cores: ", i.VCPU,
+		// 	",\tMemory(GB): ", i.Memory,
+		// 	",\tEBS Throughput(MB/s): ", i.EBSThroughput,
+		// 	",\tcost in us-east-1: ", i.Pricing["us-east-1"].Linux.OnDemand,
+		// 	",\tcost in af-south-1: ")
 
-		p := i.Pricing["af-south-1"].Linux.OnDemand
+		// p := i.Pricing["af-south-1"].Linux.OnDemand
 
-		if p == 0 {
-			fmt.Print("UNAVAILABLE")
-		} else {
-			fmt.Print(p)
-		}
+		// if p == 0 {
+		// 	fmt.Print("UNAVAILABLE")
+		// } else {
+		// 	fmt.Print(p)
+		// }
 
-		if i.Storage != nil {
-			fmt.Print(",\tLocal storage volume size(GB): ", i.Storage.Size,
-				",\tLocal storage volumes: ", i.Storage.Devices,
-				",\tLocal storage SSD: ", i.Storage.SSD)
-		}
+		// if i.Storage != nil {
+		// 	fmt.Print(",\tLocal storage volume size(GB): ", i.Storage.Size,
+		// 		",\tLocal storage volumes: ", i.Storage.Devices,
+		// 		",\tLocal storage SSD: ", i.Storage.SSD)
+		// }
 
-		fmt.Println(",\tEBS surcharge: ", i.Pricing["us-east-1"].EBSSurcharge)
+		// fmt.Println(",\tEBS surcharge: ", i.Pricing["us-east-1"].EBSSurcharge)
+
+		fmt.Print("Instance type: ", i.InstanceType,"\n")
 	}
 
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/cristim/ec2-instances-info
+module github.com/Bairagi/ec2-instances-info
 
 go 1.14
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/go-bindata/go-bindata v3.1.2+incompatible // indirect
+	github.com/pkg/errors v0.9.1
+)
